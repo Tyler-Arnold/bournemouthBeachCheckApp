1f2b348cfe1285011ac3b54ecb86091151be44b5
Tyler Arnold <arnoldTyler1998@gmail.com>
2020-11-01 20:55:35 +0000

    Typed everything west of bournemouth god help me

--- mock/beaches.tsx
+++ mock/beaches.tsx
@@ -1,23 +1,23 @@
 import { Beach } from "../types/Beach";
 
-  // const bournemouthBeachRegion: Region = {
-  //   latitude: 50.7155,
-  //   longitude: -1.875,
-  //   latitudeDelta: 0.0022,
-  //   longitudeDelta: 0.00421,
-  // };
-  // const sandbanksBeachRegion: Region = {
-  //   latitude: 50.6877,
-  //   longitude: -1.9387,
-  //   latitudeDelta: 0.0022,
-  //   longitudeDelta: 0.00421,
-  // };
-  // const boscombeBeachRegion: Region = {
-  //   latitude: 50.7195,
-  //   longitude: -1.843,
-  //   latitudeDelta: 0.0022,
-  //   longitudeDelta: 0.00421,
-  // };
+// const bournemouthBeachRegion: Region = {
+//   latitude: 50.7155,
+//   longitude: -1.875,
+//   latitudeDelta: 0.0022,
+//   longitudeDelta: 0.00421,
+// };
+// const sandbanksBeachRegion: Region = {
+//   latitude: 50.6877,
+//   longitude: -1.9387,
+//   latitudeDelta: 0.0022,
+//   longitudeDelta: 0.00421,
+// };
+// const boscombeBeachRegion: Region = {
+//   latitude: 50.7195,
+//   longitude: -1.843,
+//   latitudeDelta: 0.0022,
+//   longitudeDelta: 0.00421,
+// };
 
 const BournemouthBeachPier: Beach = {
   label: "BournemouthBeachPier",
@@ -88,10 +88,203 @@ const BournemouthBeachWest: Beach = {
   ],
 };
 
+const DurleyChineBeach: Beach = {
+  label: "DurleyChineBeach",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.71492, longitude: -1.881171 },
+    { latitude: 50.714573, longitude: -1.881054 },
+    { latitude: 50.714177, longitude: -1.882766 },
+    { latitude: 50.713926, longitude: -1.884408 },
+    { latitude: 50.713617, longitude: -1.885363 },
+    { latitude: 50.712321, longitude: -1.890505 },
+    { latitude: 50.712803, longitude: -1.890945 },
+    { latitude: 50.713513, longitude: -1.88823 },
+    { latitude: 50.7141, longitude: -1.885702 },
+  ],
+};
+
+const MiddleChineBeach: Beach = {
+  label: "MiddleChineBeach",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.71232, longitude: -1.890507 },
+    { latitude: 50.712803, longitude: -1.890943 },
+    { latitude: 50.712385, longitude: -1.892379 },
+    { latitude: 50.711937, longitude: -1.893613 },
+    { latitude: 50.71157, longitude: -1.893318 },
+    { latitude: 50.711888, longitude: -1.892178 },
+    { latitude: 50.712159, longitude: -1.89124 },
+  ],
+};
+
+const AlumChineBeach: Beach = {
+  label: "AlumChineBeach",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.71157, longitude: -1.893318 },
+    { latitude: 50.711936, longitude: -1.893613 },
+    { latitude: 50.711248, longitude: -1.8959 },
+    { latitude: 50.70923, longitude: -1.902002 },
+    { latitude: 50.708924, longitude: -1.901638 },
+    { latitude: 50.709395, longitude: -1.899853 },
+  ],
+};
+
+
+const BranksomeDeneChine: Beach = {
+  label: "BranksomeDeneChine",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.708924, longitude: -1.901638 },
+    { latitude: 50.70923, longitude: -1.902002 },
+    { latitude: 50.708452, longitude: -1.904176 },
+    { latitude: 50.707477, longitude: -1.906606 },
+    { latitude: 50.707267, longitude: -1.906402 },
+    { latitude: 50.708058, longitude: -1.904267 },
+    { latitude: 50.708618, longitude: -1.902689 },
+  ],
+};
+
+const BranksomeChine: Beach = {
+  label: "BranksomeChine",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.707267, longitude: -1.906401 },
+    { latitude: 50.707477, longitude: -1.906605 },
+    { latitude: 50.706779, longitude: -1.908734 },
+    { latitude: 50.705414, longitude: -1.911706 },
+    { latitude: 50.703824, longitude: -1.915236 },
+    { latitude: 50.703287, longitude: -1.916266 },
+    { latitude: 50.702934, longitude: -1.915558 },
+    { latitude: 50.704252, longitude: -1.91291 },
+    { latitude: 50.705509, longitude: -1.910346 },
+  ],
+};
+
+const CanfordCliffs: Beach = {
+  label: "CanfordCliffs",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.702933, longitude: -1.915559 },
+    { latitude: 50.703287, longitude: -1.916264 },
+    { latitude: 50.701707, longitude: -1.919177 },
+    { latitude: 50.700103, longitude: -1.921945 },
+    { latitude: 50.69873, longitude: -1.924048 },
+    { latitude: 50.698526, longitude: -1.923318 },
+    { latitude: 50.699831, longitude: -1.921151 },
+    { latitude: 50.701609, longitude: -1.918036 },
+  ],
+};
+
+const FlagheadChine: Beach = {
+  label: "FlagheadChine",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.698527, longitude: -1.923318 },
+    { latitude: 50.69873, longitude: -1.924047 },
+    { latitude: 50.69686, longitude: -1.926964 },
+    { latitude: 50.696541, longitude: -1.926062 },
+  ],
+};
+
+const ShoreRoad: Beach = {
+  label: "ShoreRoad",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.69686, longitude: -1.926964 },
+    { latitude: 50.694713, longitude: -1.930135 },
+    { latitude: 50.694315, longitude: -1.929181 },
+    { latitude: 50.696541, longitude: -1.926062 },
+  ],
+};
+
+const Sandbanks: Beach = {
+  label: "Sandbanks",
+  location: {
+    latitude: 50.714247,
+    longitude: -1.874762,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.00421,
+  },
+  polygon: [
+    { latitude: 50.694316, longitude: -1.929181 },
+    { latitude: 50.694713, longitude: -1.930135 },
+    { latitude: 50.693782, longitude: -1.931247 },
+    { latitude: 50.69241, longitude: -1.932964 },
+    { latitude: 50.691392, longitude: -1.934354 },
+    { latitude: 50.690589, longitude: -1.935589 },
+    { latitude: 50.689638, longitude: -1.936786 },
+    { latitude: 50.688671, longitude: -1.937883 },
+    { latitude: 50.687881, longitude: -1.938938 },
+    { latitude: 50.687375, longitude: -1.939862 },
+    { latitude: 50.686862, longitude: -1.940291 },
+    { latitude: 50.685789, longitude: -1.94145 },
+    { latitude: 50.684811, longitude: -1.942892 },
+    { latitude: 50.683782, longitude: -1.945316 },
+    { latitude: 50.683359, longitude: -1.946262 },
+    { latitude: 50.683104, longitude: -1.946659 },
+    { latitude: 50.682591, longitude: -1.945967 },
+    { latitude: 50.683235, longitude: -1.943356 },
+    { latitude: 50.684608, longitude: -1.941075 },
+    { latitude: 50.685539, longitude: -1.939388 },
+    { latitude: 50.689054, longitude: -1.935544 },
+  ],
+};
+
 const Beaches = [
   BournemouthBeachPier,
   BournemouthBeachEast,
-  BournemouthBeachWest
+  BournemouthBeachWest,
+  DurleyChineBeach,
+  MiddleChineBeach,
+  AlumChineBeach,
+  BranksomeDeneChine,
+  BranksomeChine,
+  CanfordCliffs,
+  FlagheadChine,
+  ShoreRoad,
+  Sandbanks
 ];
 
 export default Beaches;
