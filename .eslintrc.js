f42853f331aef1531faed8cb7bf0894efed91b56
Tyler Arnold <arnoldTyler1998@gmail.com>
2020-11-02 12:51:25 +0000

    switched to using google's linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  'env': {
+    'browser': true,
+    'es2021': true,
   },
-  extends: [
+  'extends': [
     'plugin:react/recommended',
-    'airbnb',
+    'google',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
+    'ecmaVersion': 12,
+    'sourceType': 'module',
   },
-  plugins: [
+  'plugins': [
     'react',
     '@typescript-eslint',
   ],
-  rules: {
+  'rules': {
   },
 };
