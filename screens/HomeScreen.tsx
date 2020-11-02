2128ece0f03a9b053321ca0515d000b2625af652
Tyler Arnold <arnoldTyler1998@gmail.com>
2020-11-02 10:50:38 +0000

    Added a basic list to the homescreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,25 +1,60 @@
-import React from "react";
-import { Button, View, StyleSheet, ViewStyle } from "react-native";
+import React, { useState } from "react";
+import { Button, View, StyleSheet, ViewStyle, Text, SafeAreaView, FlatList, StatusBar } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { HomeScreenProps } from "../types/HomeScreenProps";
+import { Beach } from "../types/Beach";
+import Beaches from "../mock/beaches";
 
 interface Styles {
   view: ViewStyle;
-  button: ViewStyle
+  button: ViewStyle;
 }
 
-const styles = StyleSheet.create<Styles>({
-    view: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "skyblue"
-    },
-    button: {
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "skyblue",
+  },
+  button: {},
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
 
-    }
-  });
+type BeachListItemProps = { beach: Beach; onPress: any; style: ViewStyle };
+
+const BeachListItem = (props: BeachListItemProps) => (
+  <TouchableOpacity onPress={props.onPress} style={[styles.item, props.style]}>
+    <Text style={styles.title}>{props.beach.label}</Text>
+  </TouchableOpacity>
+);
 
 export const HomeScreen = ({ navigation }: HomeScreenProps) => {
+  const [selectedBeach, setSelectedBeach] = useState<string|null>(null);
+
+  const renderItem = (beach: Beach) => {
+    const backgroundColor = beach.label === selectedBeach ? "#6e3b6e" : "#f9c2ff";
+
+    return (
+      <BeachListItem
+        beach={beach}
+        onPress={() => setSelectedBeach(beach.label)}
+        style={{ backgroundColor }}
+      />
+    );
+  };
+
   return (
     <View style={styles.view}>
       <Button
@@ -27,6 +62,14 @@ export const HomeScreen = ({ navigation }: HomeScreenProps) => {
         title="Go to beach"
         color="skyblue"
       />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={Beaches}
+          renderItem={(beach) => renderItem(beach.item)}
+          keyExtractor={(beach) => beach.label}
+          extraData={selectedBeach}
+        />
+      </SafeAreaView>
     </View>
   );
 };
