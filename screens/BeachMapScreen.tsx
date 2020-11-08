e71b16a14b53cd6fdaa6511c10a3b7a9c6dcbf3f
Tyler Arnold <arnoldTyler1998@gmail.com>
2020-11-08 22:53:18 +0000

    Made old map interactions area into map info drawer expands when required

--- screens/BeachMapScreen.tsx
+++ screens/BeachMapScreen.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {Button, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Button,
+  View,
+  StyleSheet,
+  Text,
+  ViewStyle,
+  StyleProp,
+} from 'react-native';
 import {BeachMapScreenProps} from '../types/BeachMapScreenProps';
 import MapView from 'react-native-maps';
 import {Beach} from '../types/Beach';
@@ -19,39 +26,107 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-evenly',
     backgroundColor: 'bisque',
+    flexDirection: 'column',
+    display: 'flex',
+  },
+  expandedinfo: {
+    flex: 0.3,
+  },
+  collapsedmap: {
+    flex: 0.7,
+  },
+  title: {
+    fontSize: 26,
+    padding: 5,
+    flex: 1,
+  },
+  buttons: {
+    flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  hidden: {
+    display: 'none',
   },
 });
 
+const MapInfo = (props: {
+  navigate: Function;
+  style: StyleProp<ViewStyle>;
+  drawerState: {
+    isInfoDrawer: boolean;
+    setIsInfoDrawer: Function;
+    toggleInfoDrawer: Function;
+  };
+}) => {
+  const BeachContain = BeachContainer.useContainer();
+
+  return (
+    <View style={props.style}>
+      <View style={styles.title}>
+        <Text
+          style={styles.title}
+          onPress={() => props.drawerState.toggleInfoDrawer()}
+        >
+          {BeachContain.currentBeach.label}
+        </Text>
+      </View>
+      <View
+        style={props.drawerState.isInfoDrawer ? styles.buttons : styles.hidden}
+      >
+        <View>
+          <Text>{'info'}</Text>
+        </View>
+        <View>
+          <Button
+            onPress={() => props.navigate('Home')}
+            title="Go Home"
+            color="blue"
+          />
+          <Button
+            onPress={() => BeachContain.setCurrentBeach(Beaches[0])}
+            title="Set Bournemouth"
+            color="brown"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
 export const BeachMapScreen = (props: BeachMapScreenProps) => {
   const CurrentBeach = BeachContainer.useContainer();
+
+  const [isInfoDrawer, setIsInfoDrawer] = useState(true);
+  const toggleInfoDrawer = () => {
+    setIsInfoDrawer(!isInfoDrawer);
+  };
 
   return (
     <>
       <MapView
         region={CurrentBeach.currentBeach.location}
-        style={styles.mapview}
+        style={[styles.mapview, isInfoDrawer ? styles.collapsedmap : null]}
       >
         <BeachPolygons
           currentBeach={CurrentBeach.currentBeach}
           favouriteBeaches={CurrentBeach.favouriteBeach}
           navigation={props}
-          handleTap={(beach: Beach) => CurrentBeach.setCurrentBeach(beach)}
+          handleTap={(beach: Beach) => {
+            setIsInfoDrawer(true);
+            return CurrentBeach.setCurrentBeach(beach);
+          }}
         />
       </MapView>
 
-      <View style={styles.interactionsview}>
-        <Button
-          onPress={() => props.navigation.navigate('Home')}
-          title="Go Home"
-          color="blue"
-        />
-        <Button
-          onPress={() => CurrentBeach.setCurrentBeach(Beaches[0])}
-          title="Set Bournemouth"
-          color="brown"
-        />
-      </View>
+      <MapInfo
+        navigate={props.navigation.navigate}
+        style={[
+          styles.interactionsview,
+          isInfoDrawer ? styles.expandedinfo : styles.interactionsview,
+        ]}
+        drawerState={{isInfoDrawer, setIsInfoDrawer, toggleInfoDrawer}}
+      ></MapInfo>
     </>
   );
 };
